1a95d986fc6a44250b4bbca536ac351fc66cba90
theycallmerah <princesstomboy37@gmail.com>
2026-05-31 22:12:52 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "MODERN BOT AKO YA!",
+        name: "MODERN BOT AKO YA!!",
         // Activity type number (0 = Playing).
         type: 1, 
       },
